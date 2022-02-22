5a734cde6711d849eae3f36d1528d8e4ce901933
Lea Renaux <lea.renaux@mail-ecv.fr>
2022-02-22 13:45:19 +0100

    add init component LocalWeather

---
+++ src/components/LocalWeather.jsx
@@ -0,0 +1,20 @@
+import React , { Component } from "react"; 
+
+class LocalWeather extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div >
+                  <p> Local weather</p>
+            </div>
+        )
+    }
+}
+
+export default LocalWeather;
