0f25560a37271ef63fd6bad9c7f5e5ba8d0bba7c
Lea Renaux <lea.renaux@mail-ecv.fr>
2022-02-23 14:44:26 +0100

    change search en classe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-//import store from "./store/store";
-//import {Provider} from "react-redux";
+import store from "./store/store";
+import {Provider} from "react-redux";
 //import './index.css';
 import App from './App';
 import Home from "./views/Home";
-import Search from "./views/Search";
+import SearchCity from "./views/Search";
 import Favorites from "./views/Favorites";
 
 const rootElement = document.getElementById("root");
@@ -14,15 +14,15 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
   <BrowserRouter>
-  {/* <Provider store={store}> */}
+  <Provider store={store}> 
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="home" element={<Home />}/>
-        <Route path="search" element={<Search />}/>
+        <Route path="search" element={<SearchCity />}/>
         <Route path="favorite" element={<Favorites />}/>
       </Route>
     </Routes>
-    {/* </Provider> */}
+  </Provider> 
   </BrowserRouter>
   </React.StrictMode>,
   rootElement
--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,69 +1,78 @@
-import {useState} from "react"
+import { Component} from "react"
 import {Input } from 'antd';
 import { connect } from "react-redux"
 import {EnvironmentOutlined} from '@ant-design/icons';
 import weatherRepository from "../repository/weatherRepository";
 import { addFavorite } from "../store/reducers/favoriteReducer"
-const Search = () => {
 
-    const { Search } = Input;
 
-    const [city, setCity] = useState("");
-    const [weatherDescription, setWeatherDescription] = useState(null);
-    const [weatherTemp, setWeatherTemp] = useState(null);
-    const [weatherIcon, setWeatherIcon] = useState(null);
-    const [listOfFavorites, setListOfFavorites] = [];
+const {Search} = Input;
 
-    const onSearch = async (city) => {
-        setCity(city)
+export class SearchCity extends Component {
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            city : "",
+            weatherDescription : null,
+            weatherTemp : null,
+            weatherIcon : null,
+            listOfFavorites : []
+            
+        } 
+    }
+
+    onSearch = async (city) => {
         const cityLocation = await weatherRepository.getLocationByCity(city);
         const latitude = cityLocation[0].lat;
-        const longitude = cityLocation[0].lon;
+        const longitude = cityLocation[0].lon; 
 
-        const cityWeather= await weatherRepository.getWeather(latitude, longitude)
-        
-        setWeatherDescription(cityWeather.weather[0].description)
-        setWeatherTemp(cityWeather.main.temp)
-        setWeatherIcon(cityWeather.weather[0].icon)
-        
+        const cityWeather= await weatherRepository.getWeather(latitude, longitude);
+        const weatherDescription = cityWeather.weather[0].description;
+        const weatherTemp = cityWeather.main.temp;
+        const weatherIcon = cityWeather.weather[0].icon;
+
+        this.setState({city : city,  weatherDescription : weatherDescription, weatherTemp : weatherTemp, weatherIcon : weatherIcon });
     }
 
     
-
-    return(
-        <>
-
-            <Search placeholder="Saisir une ville" onSearch={onSearch}  enterButton />
+    render() {
+        return(
+            <>
+                <Search placeholder="Saisir une ville" onSearch={this.onSearch}  enterButton />
             
-            <div>
-
-                {weatherDescription &&(
-                    <div>
+                <div>
+                    {this.state.weatherDescription &&(
+                        <div>
                         
-                        <h3><EnvironmentOutlined />{" "+ city}</h3>
-                        <br />
-                        <img src={"http://openweathermap.org/img/wn/"+ weatherIcon +"@2x.png"}></img>
-                        <h1>{weatherTemp + " °C"}</h1>
-                        <p>{weatherDescription}</p>
-                    </div>
-                )}
+                            <h3><EnvironmentOutlined />{" "+ this.state.city}</h3>
+                            <br />
+                            <img src={"http://openweathermap.org/img/wn/"+ this.state.weatherIcon +"@2x.png"}></img>
+                            <h1>{this.state.weatherTemp + " °C"}</h1>
+                            <p>{this.state.weatherDescription}</p>
+                            
+                        </div>
+                    )}
 
-            </div>
+                </div>
             
-        </>
-    )
-
-    const mapDispatchToProps = dispatch => {
-        return{
-            addFavorite: (favorite) => dispatch(addFavorite(favorite))
-        }
-    }
-    
-    const mapStateToProps = state => {
-        return {
-            listOfFavorites: state.favorites.listOfFavorites
-        }
+            </>
+        )
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+const mapDispatchToProps = dispatch => {
+    return{
+        addFavorite: (favorite) => dispatch(addFavorite(favorite))
+    }
+}
+    
+const mapStateToProps = state => {
+    return {
+        listOfFavorites: state.favorites.listOfFavorites
+    }
+}
+
+
+ export default connect(mapStateToProps, mapDispatchToProps)(SearchCity);
+// export default Search;
